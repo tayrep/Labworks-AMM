2c8f1934c4fb6be42cb70b3b4c60c50102d54c16
Евгений Федечкин <shadowtheeugene@gmail.com>
2024-09-30 14:10:45 +0300

    Лаба по принадлежности точки закрашенной области

---
+++ 3_labФигура/3_labФигура.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    float x, y;
+
+    cout << "Введите координату (x) точки на плоскости: ";
+    cin >> x;
+    cout << "Введите координату (y) той же точки: ";
+    cin >> y;
+
+    if (x * x + y * y <= 4 && (x <= 0 || y >= 2 * x)) {
+        cout << "Принадлежит" << endl;
+    }
+    else {
+        cout << "Не принадлежит" << endl;
+    }
+}
